053ee17381520f6bd158473d6f96de28a4e9f554
Vinícius dos Santos Tavares <vini4812@hotmail.com.br>
2022-11-02 17:23:25 -0300

    Adição de user e extensões.

---
+++ Api/Api/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Userpassword { get; set; }
+    }
+}
